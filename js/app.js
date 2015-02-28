a10e362b998390f08a6462377634c340c709982e
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2015-02-28 17:31:33 +0530

    searchable table using vanilla JS

---
+++ js/app.js
@@ -0,0 +1,79 @@
+var details=[
+	{
+		id:"1",
+		name:"xx",
+		age:45,
+		occupation:"clerk"
+	},
+	{
+		id:"2",
+		name:"Jane Doe",
+		age:65,
+		occupation:"manager"
+	},
+	{
+		id:"1",
+		name:"John Smith",
+		age:21,
+		occupation:"assitant"
+	},
+	{
+		id:"1",
+		name:"Rohit Kumar",
+		age:19,
+		occupation:"intern"
+	},
+	{
+		id:"1",
+		name:"John Doe",
+		age:45,
+		occupation:"clerk"
+	}
+];
+var generator=function(obj,tableID)
+{
+	var table="<table border='1'><tr><th>sn</th><th>name</th><th>age</th><th>occupation</th></tr>";
+	obj.forEach(function(e,r,b)
+	{
+		var td="<tr><td>"+e.id+"</td>";
+		td+="<td>"+e.name+"</td>";
+		td+="<td>"+e.age+"</td>";
+		td+="<td>"+e.occupation+"</td></tr>";
+		table+=td;
+	});
+	table+="</table>";
+	document.getElementById(tableID).innerHTML=table;
+};
+var finder=function(obj,filterText)
+{
+	var newObj=[];
+	obj.forEach(function(e,r,b)
+	{
+		var push=false;
+		var clone=e;
+		for(var i in clone){
+				var field=clone[i].toString();
+				var match=field.match(new RegExp(filterText,"i"));
+				if(match!==null)
+				{
+					push=true;
+				}
+			}
+		if(push){
+			newObj.push(clone);
+		}
+	});
+	return newObj;
+};
+var search=function(e)
+{
+	var text=(this.value);
+	var found=finder(details,text);
+	generator(found,"table-wrapper");
+};
+window.onload=function()
+{
+	generator(details,"table-wrapper");
+	document.getElementById("search-text")
+			.addEventListener("keyup",search);
+};
